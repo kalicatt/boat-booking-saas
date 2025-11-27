b35435e907d328a5e7e271b879ff03dbb3ea6de8
kalicatt <servaislucas68@gmail.com>
2025-11-27 01:53:32 +0100

    feat: configure image optimization settings to disable Next.js optimizer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // Workaround 400 Bad Request on Next image optimizer in prod
+    // We serve from /public so disabling optimizer is fine
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
