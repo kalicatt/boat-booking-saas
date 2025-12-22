697fefbd3bab53cb0640a44af3f2ffe742c68e37
kalicatt <servaislucas68@gmail.com>
2025-12-22 14:39:18 +0100

    fix: use sed instead of envsubst for alertmanager config

--- monitoring/alertmanager-entrypoint.sh
+++ monitoring/alertmanager-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
-# Substitute environment variables in alertmanager config
-envsubst '${SMTP_HOST} ${SMTP_PORT} ${SMTP_USER} ${SMTP_PASS} ${ALERT_WEBHOOK_URL} ${DISCORD_WEBHOOK_URL}' \
-  < /etc/alertmanager/alertmanager.yml.template \
-  > /etc/alertmanager/alertmanager.yml
+# Substitute environment variables in alertmanager config using sed
+sed -e "s|\${SMTP_HOST}|${SMTP_HOST}|g" \
+    -e "s|\${SMTP_PORT}|${SMTP_PORT}|g" \
+    -e "s|\${SMTP_USER}|${SMTP_USER}|g" \
+    -e "s|\${SMTP_PASS}|${SMTP_PASS}|g" \
+    -e "s|\${ALERT_WEBHOOK_URL}|${ALERT_WEBHOOK_URL}|g" \
+    -e "s|\${DISCORD_WEBHOOK_URL}|${DISCORD_WEBHOOK_URL}|g" \
+    /etc/alertmanager/alertmanager.yml.template > /etc/alertmanager/alertmanager.yml
 
 # Start alertmanager
 exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml "$@"
